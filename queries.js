500c7c6683f693b5b6ecd2901740ea0f818ba913
Bill Wilke <billwilke2@gmail.com>
2020-11-25 11:04:16 -0700

    Adds title to table

--- queries.js
+++ queries.js
@@ -7,34 +7,25 @@ const pool = new Pool({
   port: 5432,
 })
 
-// app.get('/api/v1/movie/:imdb_ID/ratings', (request, response) => {
-//   const imdbID = request.params.imdb_ID
-//   console.log(app.locals.ratings)
-//   const ratings = app.locals.ratings.find(rating => rating.imdb_ID === imdbID)
-//   if(!ratings) {
-//     // app.locals.ratings.push({imdb_ID: imdbID, thumbs_up: 0, thumbs_down: 0})
-//     return response.status(200).json({imdb_ID: imdbID, thumbs_up: 0, thumbs_down: 0})
-//   }
-//   return response.status(200).json(ratings)
-// })
-const createRating = (imdb_id, thumbs_up, thumbs_down, response ) => {
+const createRating = (imdb_id, thumbs_up, thumbs_down, title, response ) => {
   // const { imdb_id, thumbs_up, thumbs_down } = request.body
-  pool.query('INSERT INTO ratings (imdb_id, thumbs_up, thumbs_down) VALUES ($1, $2, $3)', 
-  [imdb_id, thumbs_up, thumbs_down],
+  pool.query('INSERT INTO ratings (imdb_id, thumbs_up, thumbs_down, title) VALUES ($1, $2, $3, $4)', 
+  [imdb_id, thumbs_up, thumbs_down, title],
   (error, results) => {
     if (error) {
       throw error
     }
-    response.status(200).json([{imdb_id: imdb_id, thumbs_up: 0, thumbs_down: 0}])
+    response.status(200).json([{imdb_id: imdb_id, thumbs_up: 0, thumbs_down: 0, title: ''}])
   })
 }
 
 const getMovieById = (request, response) => {
   const imdbID = request.params.imdb_id;
+  // const title = request.params.title
   console.log(imdbID)
   pool.query('SELECT * FROM ratings WHERE imdb_id = $1', [imdbID], (error, results) => {
     if(results.rows.length === 0) {
-      createRating(imdbID, 0, 0, response)
+      createRating(imdbID, 0, 0, '', response)
       // response.status(200).json([{imdb_id: imdbID, thumbs_up: 0, thumbs_down: 0}])
     } else if (error) {
       throw error
@@ -45,18 +36,16 @@ const getMovieById = (request, response) => {
 }
 
 const addRating = (request, response) => {
-  const { imdb_id, thumbs_up, thumbs_down } = request.body
+  const { imdb_id, thumbs_up, thumbs_down, title } = request.body
+  console.log(request.body)
   pool.query(
-    'UPDATE ratings SET thumbs_up = $1, thumbs_down = $2 WHERE imdb_id = $3',
-    [thumbs_up, thumbs_down, imdb_id ],
+    'UPDATE ratings SET thumbs_up = $1, thumbs_down = $2, title = $3 WHERE imdb_id = $4',
+    [thumbs_up, thumbs_down, title, imdb_id ],
     (error, results) => {
-      if(results.rows.length === 0) {
-
-      } 
       if (error) {
         throw error
       }
-      response.status(200).json({imdb_id: `${imdb_id}`, thumbs_up: `${thumbs_up}`, thumbs_down: `${thumbs_down}`})
+      response.status(200).json({imdb_id: `${imdb_id}`, thumbs_up: `${thumbs_up}`, thumbs_down: `${thumbs_down}`, title: `${title}`})
     }
   )
 }
